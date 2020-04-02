ba6107bf9547fb389748ae709e927af6232ce7ec
ziisp <895512233@qq.com>
2020-04-02 16:38:40 +0800

    first commit

---
+++ wp-content/themes/b2child/child.js
@@ -0,0 +1,8 @@
+//您自己的js代码写到下面
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2a2789e0cc9fc823e85d9cbf75234b52";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
